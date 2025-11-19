cda61aa35618c89f588f6e9c0f7d4df525a207df
Justicartem <justicefinderxd@gmail.com>
2025-11-19 05:28:21 +0100

    minor fix

--- examples/05_download_stuff.py
+++ examples/05_download_stuff.py
@@ -5,7 +5,7 @@ from atomic_writer import SimpleWriter
 url = "https://fsn1-speed.hetzner.com/100MB.bin"
 with requests.get(url, stream=True) as r:
 	r.raise_for_status()
-	with SimpleWriter("examples_out/1MB.bin", mode="wb") as f:
+	with SimpleWriter("results/100MB.bin", mode="wb") as f:
 		for chunk in r.iter_content(1024*64):
 			if chunk:
 				f.write(chunk)
